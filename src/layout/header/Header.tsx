26318ad519381ca6e15869dd600e72d6081e706e
Vad197779 <kovtunvad@gmail.com>
2023-08-23 18:03:52 +0300

    Header

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import styled from 'styled-components';
 import { Logo } from '../../components/logo/Logo';
-import { Menu } from '../../components/logo/Menu';
 import { Container } from '../../components/Container';
 import { FlexContainer } from '../../components/FlexContainer';
+import { HeaderMenu } from './headerMenu/HeaderMenu';
 
 const items = ["Home", "Skills", "Works", "Testimony", "Contact"]
 
@@ -13,7 +13,7 @@ export const Header = () => {
       <Container>
         <FlexContainer justify={"space-between"} align={"center"}>
           <Logo />
-          <Menu menuItems={items}/>
+          <HeaderMenu menuItems={items}/>
         </FlexContainer>
       </Container>
     </StyledHeader>
@@ -21,6 +21,12 @@ export const Header = () => {
 };
 
 const StyledHeader = styled.header`
-  background-color: #42e8e0;
+  background-color: rgba(31, 31, 32, 0.9);
+  padding: 20px 0;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 99999;
 `
 
