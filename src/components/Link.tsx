389d842cd18e498539257916d31f4d25865b0ccf
Vad197779 <kovtunvad@gmail.com>
2023-08-24 14:19:59 +0300

    Testimony

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,35 +1,30 @@
 import styled from "styled-components";
 import { theme } from "../styles/Theme";
 
-export const Button = styled.button`
+export const Link = styled.a`
   font-size: 14px;
   font-weight: 400;
   letter-spacing: 1px;
   text-transform: uppercase;
-  width: 170px;
-  height: 32px;
-
+  padding: 10px;
   position: relative;
   z-index: 0;
 
   &:hover {
     &::before {
-      width: 100%;
-      height: 100%;
+      height: 12px;
     }
   }
 
   &::before {
     content: "";
     display: inline-block;
-    width: 50%;
-    height: 10px;
-    background-color: ${theme.colors.accent};
 
     position: absolute;
-    left: 50%;
-    bottom: 0;
-    transform: translateX(-50%);
+    bottom: 5px;
+    left: 0px;
+    right: 0px;
+    background-color: ${theme.colors.accent};
     z-index: -1;
   }
 `
