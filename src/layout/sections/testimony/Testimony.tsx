13fd7e89753b8492a05c46b1449ba2ee91c89546
Vad197779 <kovtunvad@gmail.com>
2023-08-24 15:29:39 +0300

    Contact

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -4,13 +4,16 @@ import { SectionTitle } from '../../../components/SectionTitle';
 import { Icon } from '../../../components/icon/Icon';
 import { Slider } from '../../../components/slider/Slider';
 import { FlexContainer } from '../../../components/FlexContainer';
+import { IconWrapper } from '../skills/skill/Skill';
 
 export const Testimony = () => {
   return (
     <StyledTestimony>
       <SectionTitle>Testimony</SectionTitle>
       <FlexContainer direction={"column"} align={"center"}>
-        <Icon iconId={"quote"}/>
+        <IconWrapper>
+          <Icon iconId={"quote"}/>
+        </IconWrapper>
         <Slider />
       </FlexContainer>
     </StyledTestimony>
@@ -18,6 +21,8 @@ export const Testimony = () => {
 };
 
 const StyledTestimony = styled.section`
-  min-height: 50vh;
-  background-color: #84acd3;
+
+  ${IconWrapper} {
+    margin: 48px 0 72px;
+  }
 `
